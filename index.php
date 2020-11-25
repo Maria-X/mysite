d5f9f45e9e79a4913ca49e1c67af5c76dd5b26cb
unknown <harluginafly8@gmail.com>
2020-11-25 17:52:14 +0700

    initial code commit

---
+++ index.php
@@ -0,0 +1,38 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/home.css" rel="stylesheet" type="text/css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Coda:wght@400;800&display=swap" rel="stylesheet">
+        <title>start</title>
+    </head>
+    <body>
+      <div class="background-video-container">
+         <video class="background-video-element" autoplay muted loop>
+         <source src="assets/background_video.mp4" />
+         </video> 
+       <img class="background-video-image" src="assets/background_video_image.png" /> 
+       <div class="background-video-overlay"></div> 
+      <div class="background-video-text-overlay">
+        Hello, friend!
+       </div>
+      </div>
+      <div class="content">
+      <div class="content">
+      <div class="content-inner">
+      <div class="content-inner-padding">
+         <h1>Welcome to my admin panel</h1>
+          <div>
+          <div>
+						This is the website where you, the users, decide what features get added. Comment on any of my videos and let me know what you want to learn and see implemented! I will constantly be building on this website, adding features and creating videos based on what you, the users, want to see and learn.
+					</div>
+          </div>      
+       </div>
+      </div>
+      </div>
+      <div>
+    </body>
+    </html>
+ 
